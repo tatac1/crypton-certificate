c4be5423ed506f9ef72be9ec1225b11ca9ddc5cc
Kazu Yamamoto <kazu@iij.ad.jp>
2025-10-23 18:06:12 +0900

    set cloexec on Unix

--- x509-store/Data/X509/File.hs
+++ x509-store/Data/X509/File.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 module Data.X509.File (
     readSignedObject,
     readKeyFile,
@@ -14,6 +16,9 @@ import Data.Maybe
 import Data.PEM (PEM, pemContent, pemName, pemParseLBS)
 import qualified Data.X509 as X509
 import Data.X509.Memory (pemToKey)
+#if !defined(mingw32_HOST_OS)
+import System.Posix.IO
+#endif
 
 newtype PEMError = PEMError {displayPEMError :: String}
     deriving (Show)
@@ -23,7 +28,13 @@ instance Exception PEMError where
 
 readPEMs :: FilePath -> IO [PEM]
 readPEMs filepath = do
+#if defined(mingw32_HOST_OS)
     content <- L.readFile filepath
+#else
+    fd <- openFd filepath ReadOnly defaultFileFlags{cloexec = True}
+    h <- fdToHandle fd
+    content <- L.hGetContents h
+#endif
     either (throw . PEMError) pure $ pemParseLBS content
 
 -- | return all the signed objects in a file.
